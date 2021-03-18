85b35b3843145a9506b7fb5e467dbff1a44cff24
Manuel Kießling <manuel@kiessling.net>
2021-03-18 12:08:59 +0100

    streamfile.sh client now pushes new events every N events OR, if no new events are incoming, after M seconds

--- client/bash/streamfile.sh
+++ client/bash/streamfile.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-filepath="$1"
+trap "kill 0" SIGINT
+
+filePath="$1"
 source="$2"
 userId="$3"
 serverId="$4"
 apiKeyId="$5"
 bufferSize="$6"
+controlTimeout="$7"
+
+controlFilePath="$(mktemp)"
+controlFileEntry="--- ServerLogger.com streamfile.sh control file entry ---"
+
+echo "Control file path: $controlFilePath"
+
+while true; do
+  sleep "$controlTimeout"
+  echo "$controlFileEntry" >> "$controlFilePath"
+done &
 
 source="${source:0:256}"
 
@@ -13,11 +26,15 @@ i=0
 eventsBuffer=()
 eventsString=""
 
-tail -n0 -F "$filepath" | while read -r line; do
-  line="${line:0:512}"
-  eventsBuffer[${#eventsBuffer[@]}]='{"source":"'"$source"'", "createdAt":"'"$(date +"%Y-%m-%dT%H:%M:%S%z")"'", "payload":"'"$(echo $line | sed "s/\"/\\\\\"/g")"'"}'
 
-  if [ "$i" == "$bufferSize" ]
+tail -n0 -F -q "$filePath" "$controlFilePath" | while read -r line; do
+  line="${line:0:512}"
+  if [ "$line" != "$controlFileEntry" ]
+  then
+    eventsBuffer[${#eventsBuffer[@]}]='{"source":"'"$source"'", "createdAt":"'"$(date +"%Y-%m-%dT%H:%M:%S%z")"'", "payload":"'"$(echo $line | sed "s/\"/\\\\\"/g")"'"}'
+  fi
+
+  if [ "$i" = "$bufferSize" ] || [ "$line" = "$controlFileEntry" ]
   then
     j=0
     for eventString in "${eventsBuffer[@]}"
@@ -32,18 +49,21 @@ $eventString"
       j=$((j+1))
     done
 
-    date
-    echo "$eventsString"
-    curl \
-      -X POST \
-      "https://rs213s9yml.execute-api.eu-central-1.amazonaws.com/server-events" \
-      -d '{ "userId":   "'"$userId"'",
-            "serverId": "'"$serverId"'",
-            "apiKeyId": "'"$apiKeyId"'",
-            "events":   ['"$eventsString"']}'
+    if [ "$j" != 0 ]
+    then
+      date
+      echo "$eventsString"
+      curl \
+        -X POST \
+        "https://rs213s9yml.execute-api.eu-central-1.amazonaws.com/server-events" \
+        -d '{ "userId":   "'"$userId"'",
+              "serverId": "'"$serverId"'",
+              "apiKeyId": "'"$apiKeyId"'",
+              "events":   ['"$eventsString"']}'
 
-      i=0
-      eventsBuffer=()
+        i=0
+        eventsBuffer=()
+    fi
 
   else
     i=$((i+1))
